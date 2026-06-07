5a5eca00d0ad9d65000083a908a94fd57efa59e0
Konstantin Adamov <xrayadamo@gmail.com>
2026-06-06 18:49:32 -0700

    Add save to file feature. Add new translations and fix old ones.

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,7 +1,11 @@
-pub mod constants;
 // (c) 2025,26 Konstantin Adamov, licensed under MIT
 
 pub mod actions;
+pub mod constants;
 pub mod i18n;
 pub mod init_icon;
 pub mod number_editor;
+
+pub use actions::{AboutAction, WindowActionGroup, create_about_action};
+pub use constants::{APP_NAME, APP_ID, SPACING_LARGE, SPACING_MEDIUM};
+pub use number_editor::NumberEditor;
